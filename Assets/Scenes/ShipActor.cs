e2b1db0a5fa8d8ded8ef2d7ae3dd31be9f6dc372
Ashish Aapan <ashishaapan@gmail.com>
2018-05-28 20:00:58 +0530

    added correction force,stabilizer and reorganized code

--- Assets/Scenes/ShipActor.cs
+++ Assets/Scenes/ShipActor.cs
@@ -11,6 +11,8 @@ public class ShipActor : MonoBehaviour{
 	public float forceFactor = 1;
 	public float rotationFactor = 10;
 	public float gravityFactor = 0.5f;
+	public float reactive = 0.03f;
+	
 	
 	
 	// Use this for initialization
@@ -24,13 +26,35 @@ public class ShipActor : MonoBehaviour{
 		
 		
 		
+		
+		
+		
 		myRigidbody.AddForce(-Physics.gravity*gravityFactor);
+
+		
+		thrust();
+		rotate();
+		correct();
+		
+		
+	
+			
+
+
+		
+	}
+
+
+	private void thrust(){
+		
 		if (Input.GetKey(KeyCode.W)){
 			myRigidbody.AddRelativeForce(Vector3.up*forceFactor,ForceMode.Force);	
 		}
-		
-		
-		
+
+
+	}
+	private void rotate(){
+				
 		if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)){		
 			myRigidbody.AddTorque(Vector3.forward*rotationFactor);
 		}
@@ -40,8 +64,18 @@ public class ShipActor : MonoBehaviour{
 			myRigidbody.AddTorque(-Vector3.forward*rotationFactor);
 		}
 
-
-		
 	}
+
+	private void correct(){
+		
+		float len = myRigidbody.velocity.magnitude;
+		Vector3 velocityNorm = myRigidbody.velocity.normalized;
+		Vector3 proj = Vector3.Project(velocityNorm, transform.up);
+		
+		//exponential correction factor
+		myRigidbody.AddForce(-reactive * (myRigidbody.velocity - proj * len), ForceMode.Impulse);
+		Debug.DrawLine(myRigidbody.position, myRigidbody.position + myRigidbody.velocity, Color.black, 0.0f, false);
+	}
+	
 	private Rigidbody myRigidbody;
 }
